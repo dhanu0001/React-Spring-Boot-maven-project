3339fb3494c50a395ecf5ec718447d2d48d40d34
Tejsingh Dhaosriya <tejsinghdhaosriya@gmail.com>
2021-05-20 17:56:10 +0530

    [FE]-updated for dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import ListEmployeeComponent from './components/ListEmployeeComponent';
-import HeaderComponent from './components/HeaderComponent';
-import FooterComponent from './components/FooterComponent';
-import CreateEmployeeComponent from './components/CreateEmployeeComponent';
-import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
-import ViewEmployeeComponent from './components/ViewEmployeeComponent';
+import React from "react";
 
-function App() {
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Employees from "./modules/Employees/index";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+
+import { withTheme } from "./theme";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    height: "100%",
+    [theme.breakpoints.down("xs")]: {
+      paddingTop: theme.spacing(2), // Material UI spacing
+    },
+    position: "absolute",
+    overflow: "auto",
+    backgroundColor: theme.palette.background.paper, // Makes it the same color as the paper color
+    // backgroundImage: `url(${"https://www.transparenttextures.com/patterns/cubes.png"})`,
+  },
+}));
+
+function App(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const { darkMode, setDarkMode } = props;
+
   return (
-    <div>
-        <Router>
-              <HeaderComponent />
-                <div className="container">
-                    <Switch> 
-                          <Route path = "/" exact component = {ListEmployeeComponent}></Route>
-                          <Route path = "/employees" component = {ListEmployeeComponent}></Route>
-                          <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
-                          <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}></Route>
-                          {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
-                    </Switch>
-                </div>
-              <FooterComponent />
-        </Router>
-    </div>
-    
+    <Grid className={classes.root} container direction="column">
+      <Grid item container justify="flex-end">
+        <FormControlLabel
+          control={
+            <Switch
+              checked={darkMode}
+              onChange={() => setDarkMode(!darkMode)}
+            />
+          }
+          label="Dark Mode"
+        />
+      </Grid>
+      <Grid item>
+        <Employees />
+      </Grid>
+    </Grid>
   );
 }
 
-export default App;
+export default withTheme(App);
