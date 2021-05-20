41db51fec4a611d76a0fe7bab944dc3bad661186
Tejsingh Dhaosriya <tejsinghdhaosriya@gmail.com>
2021-05-21 03:47:40 +0530

    [FE]-added employee list and detail

---
+++ frontend/src/modules/Employees/EmployeeDetail.js
@@ -0,0 +1,166 @@
+import React from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+import { Formik, Form, Field } from "formik";
+import { LinearProgress } from "@material-ui/core";
+import { TextField } from "formik-material-ui";
+import CloseIcon from "@material-ui/icons/Close";
+
+import {
+  selectDrawer,
+  selectEmployee,
+  setDrawer,
+  setEmployee,
+} from "./employeesSlice";
+import { postEmployee, putEmployee } from "./actions";
+const useStyles = makeStyles({
+  list: {
+    width: 400,
+  },
+  fullList: {
+    width: "auto",
+  },
+});
+
+const EmployeeDetail = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const employee = useSelector(selectEmployee);
+  const drawer = useSelector(selectDrawer);
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
+      })}
+      role="presentation"
+    >
+      <Formik
+        initialValues={{
+          firstName: employee?.firstName || "",
+          lastName: employee?.lastName || "",
+          emailId: employee?.emailId || "",
+        }}
+        enableReinitialize="true"
+        validate={(values) => {
+          const errors = {};
+
+          // first name
+          if (!values.firstName) {
+            errors.firstName = "Required";
+          }
+
+          // last name
+          if (!values.lastName) {
+            errors.lastName = "Required";
+          }
+
+          // emailId
+          if (!values.emailId) {
+            errors.emailId = "Required";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          if (employee?.id) {
+            dispatch(putEmployee({ values, id: employee?.id, setSubmitting }));
+          } else {
+            dispatch(postEmployee({ values, setSubmitting }));
+          }
+        }}
+      >
+        {({ submitForm, isSubmitting }) => (
+          <Form>
+            <Grid container alignItems="center" direction="column" spacing={1}>
+              <Grid item container direction="column" spacing={1}>
+                <Button
+                  onClick={() => dispatch(setDrawer(false))}
+                  style={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    margin: "1rem",
+                  }}
+                >
+                  <CloseIcon />
+                </Button>
+              </Grid>
+              <Grid item>
+                {!!employee?.id === false ? "Create" : "Update"} Employee
+              </Grid>
+              <Grid item>
+                <Field
+                  component={TextField}
+                  name="firstName"
+                  type="text"
+                  label=" First Name"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item>
+                <Field
+                  component={TextField}
+                  name="lastName"
+                  type="text"
+                  label=" Last Name"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item>
+                <Field
+                  component={TextField}
+                  name="emailId"
+                  type="text"
+                  label=" emailId"
+                  variant="outlined"
+                />
+              </Grid>
+              {isSubmitting && <LinearProgress />}
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={submitForm}
+                size="large"
+              >
+                {!!employee?.id === false ? "Create " : "Update "}
+                Employee
+              </Button>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+
+  return (
+    <Box>
+      <Grid container direction="column">
+        <Grid item>
+          <Button
+            onClick={() => {
+              dispatch(setDrawer(!drawer));
+              dispatch(setEmployee());
+            }}
+            color="primary"
+            variant="contained"
+          >
+            Add Employee
+          </Button>
+          <Drawer
+            anchor={"right"}
+            open={drawer}
+            onClose={() => dispatch(setDrawer(false))}
+          >
+            {list("right")}
+          </Drawer>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default EmployeeDetail;
---
+++ frontend/src/modules/Employees/EmployeesList.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import Button from "@material-ui/core/Button";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import DeleteIcon from "@material-ui/icons/Delete";
+import CreateIcon from "@material-ui/icons/Create";
+import { useDispatch, useSelector } from "react-redux";
+import {  selectEmployees, setDrawer } from "./employeesSlice";
+import {
+  deleteEmployee,
+  getEmployee,
+  getEmployees,
+} from "./actions";
+
+const useStyles = makeStyles({
+  table: {
+    overflow:"hidden"
+  },
+});
+
+
+
+export default function EmployeesList() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const employees = useSelector(selectEmployees);
+
+  useEffect(() => {
+    dispatch(getEmployees());
+  }, []);
+  return (
+    <TableContainer component={Paper} style={{width:"99vw"}}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Employee First Name</TableCell>
+            <TableCell align="right">Employee Last Name</TableCell>
+            <TableCell align="right">Employee Email Id</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {employees.map((row) => (
+            <TableRow key={row.firstName}>
+              <TableCell component="th" scope="row">
+                {row.firstName}
+              </TableCell>
+              <TableCell align="right">{row.lastName}</TableCell>
+              <TableCell align="right">{row.emailId}</TableCell>
+              <TableCell align="right">
+                <Button onClick={() => { dispatch(getEmployee(row.id));dispatch(setDrawer(true))}}>
+                  <CreateIcon />
+                </Button>
+                <Button onClick={() => dispatch(deleteEmployee(row.id))}>
+                  <DeleteIcon />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
